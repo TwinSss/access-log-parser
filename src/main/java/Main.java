b1e285246315acd03d52ec3b39b913c801c4f852
Sergei Nazarov <chulan4ikmoi91@yandex.ru>
2023-07-14 14:03:18 +0300

    Класс Main. Добавлены исключения и догика для считывания файла по строкам.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +6,11 @@ public class Main {
     public static void main(String[] args) {
 
         int count = 0;
+        String line;
+        int light;
+        int countLine = 0;
+        int minLine = Integer.MAX_VALUE;
+        int maxLine = Integer.MIN_VALUE;
 
         while (true) {
             String path = new Scanner(System.in).nextLine();
@@ -24,6 +29,25 @@ public class Main {
                 System.out.println("Указанный путь не является путём к файлу");
                 continue;
             }
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                countLine = 0;
+                while ((line = reader.readLine()) != null) {
+                    countLine++;
+                    light = line.length();
+                    if (light > 1024) throw new RuntimeException("Длинна строки больше 1024 символа");
+                    if (light < minLine) minLine = light;
+                    if (light > maxLine) maxLine = light;
+                }
+            } catch (FileNotFoundException ex) {
+                ex.getStackTrace();
+            } catch (IOException ex) {
+                System.err.println("Ошибка при чтении файла: " + ex.getMessage());
+            }
+            System.out.println("Общее количество строк в файле: " + countLine);
+            System.out.println("Длина самой длинной строки в файле: " + maxLine);
+            System.out.println("длину самой короткой строки в файле: " + minLine);
         }
     }
 }
