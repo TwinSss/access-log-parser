ec68c4f75b5e07c019d3428df7e0f1ff48a1ad5e
Sergei Nazarov <chulan4ikmoi91@yandex.ru>
2023-07-31 18:41:59 +0300

    Класс Main. Добавлена логика подсчета количества запросов от YandexBot и GoogleBot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,33 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String parseLine(String line) {
+        String userAgent = null;
+
+        String[] getFragment = line.split("\"");
+
+        if (getFragment[5].length() > 1) {
+            int firstIndex = getFragment[5].indexOf("(");
+            int lastIndex = getFragment[5].indexOf(")");
+            if (firstIndex == -1 || lastIndex == -1) return null;
+            String mass = getFragment[5].substring(++firstIndex, lastIndex);
+            String[] getFragmentUserAgent = mass.split(";");
+            if (getFragmentUserAgent.length > 1) {
+                String[] getUserAgent = getFragmentUserAgent[1].split("/");
+                userAgent = getUserAgent[0].substring(1);
+                System.out.println(userAgent);
+            }
+        }
+        return userAgent;
+    }
+
     public static void main(String[] args) {
 
         int count = 0;
-        String line;
+        String line, dataBot;
         int light;
-        int countLine = 0;
-        int minLine = Integer.MAX_VALUE;
-        int maxLine = Integer.MIN_VALUE;
+        int countLine = 0, countYandexBot = 0, countGoogleBot = 0;
+
 
         while (true) {
             String path = new Scanner(System.in).nextLine();
@@ -34,11 +53,12 @@ public class Main {
                 BufferedReader reader = new BufferedReader(fileReader);
                 countLine = 0;
                 while ((line = reader.readLine()) != null) {
+                    dataBot = Main.parseLine(line);
+                    if (dataBot != null && dataBot.toLowerCase().equals("yandexbot")) countYandexBot++;
+                    if (dataBot != null && dataBot.toLowerCase().equals("googlebot")) countGoogleBot++;
                     countLine++;
                     light = line.length();
                     if (light > 1024) throw new RuntimeException("Длинна строки больше 1024 символа");
-                    if (light < minLine) minLine = light;
-                    if (light > maxLine) maxLine = light;
                 }
             } catch (FileNotFoundException ex) {
                 ex.getStackTrace();
@@ -46,8 +66,8 @@ public class Main {
                 System.err.println("Ошибка при чтении файла: " + ex.getMessage());
             }
             System.out.println("Общее количество строк в файле: " + countLine);
-            System.out.println("Длина самой длинной строки в файле: " + maxLine);
-            System.out.println("длину самой короткой строки в файле: " + minLine);
+            System.out.println("Общее количество запросов GoogleBot в файле: " + countGoogleBot);
+            System.out.println("Общее количество запросов YandexBot в файле: " + countYandexBot);
         }
     }
 }
