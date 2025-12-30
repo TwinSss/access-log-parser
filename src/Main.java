c38e0fc340e9a485ff6753706582324991e7990c
Sergei Nazarov <snazarov@inno.tech>
2025-12-30 16:03:36 +0300

    updated file Main.java (added logic for math calc)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int numOne = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int numTwo = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел: " + (numOne + numTwo));
+        System.out.println("Разность чисел: " + (numOne - numTwo));
+        System.out.println("Произведение чисел: " + (numOne * numTwo));
+        System.out.println("Частное чисел: " + (double)(numOne / numTwo));
     }
 }
