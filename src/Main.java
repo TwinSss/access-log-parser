97df7aa26d8cf5da04e57843ba90a96a871a605b
Sergei Nazarov <snazarov@inno.tech>
2026-01-26 17:37:03 +0300

    updated Main.java (added file path checking)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+
+        int countFiles = 0;
+        //noinspection InfiniteLoopStatement
+        while (true) {
+            System.out.println("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists) {
+                System.out.println("Указанный файл не существует.");
+                continue;
+            } else if (isDirectory) {
+                System.out.println("Указанный путь является папкой.");
+                continue;
+            } else {
+                countFiles++;
+                System.out.println("Путь указан верно.");
+                System.out.println("Файл номер - " + countFiles);
+            }
+        }
     }
 }
