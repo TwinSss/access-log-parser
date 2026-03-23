7d7ff1f67a957bb7630f446d2c2fd1c55b25eee7
Sergei Nazarov <snazarov@inno.tech>
2026-03-23 17:15:51 +0300

    updated Main.java (added exception when processing file)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -13,6 +13,11 @@ public class Main {
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
+            int maxLength = Integer.MIN_VALUE;
+            int minLength = Integer.MAX_VALUE;
+            int countLine = 0;
+            String line;
+            int length;
 
             if (!fileExists) {
                 System.out.println("Указанный файл не существует.");
@@ -24,6 +29,22 @@ public class Main {
                 countFiles++;
                 System.out.println("Путь указан верно.");
                 System.out.println("Файл номер - " + countFiles);
+
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    while ((line = reader.readLine()) != null) {
+                        length = line.length();
+                        countLine++;
+                        if (length > 1024) throw new LineLongException("Длина строки " + countLine + " в файле: "
+                                + length + ". Максимально допустимая длина 1024 символа!");
+                        else if (length < minLength) minLength = length;
+                        else if (length > maxLength) maxLength = length;
+                    }
+                    System.out.println("Длина самой длинной строки в файле: " + maxLength);
+                    System.out.println("Длина самой короткой строки в файле: " + minLength);
+                    System.out.println("Общее количество строк в файле: " + countLine);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
